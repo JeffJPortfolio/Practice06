23a95ffcab9aa774ffd4fca3d4f72dfb06b359bd
JeffJPortfolio <jeffjeon.official@gmail.com>
2024-07-09 17:45:45 +0900

    ex06

--- src/com/javaex/ex06/MyBase.java
+++ src/com/javaex/ex06/MyBase.java
@@ -1,7 +1,22 @@
 package com.javaex.ex06;
 
 public class MyBase extends Base{
+	
+	public void service(String state) {
+		if (state.equals("오후")) {
+			noon();
+		}
+		else {
+			super.service(state);
+		}
+		
+    }
+	@Override
+    public void day() {
+        System.out.println("낮에는 열심히 수업을 듣자");
+    }
 
-    //코드작성
-    
+    public void noon() {
+    	System.out.println("오후도 낮과 마찬가지로 공부해야 합니다");
+    }
 }

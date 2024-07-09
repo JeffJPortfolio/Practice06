9b2767e0dd7e58eab557ff27803530de2fb15506
JeffJPortfolio <jeffjeon.official@gmail.com>
2024-07-09 17:02:08 +0900

    ex05

--- src/com/javaex/ex05/Depart.java
+++ src/com/javaex/ex05/Depart.java
@@ -1,7 +1,22 @@
 package com.javaex.ex05;
 
-public class Depart {
+public class Depart extends Employee{
 
 	 //코드작성
-
+	private String department;
+	
+	public Depart() {
+		
+	}
+	
+	public Depart(String n,int s, String d) {
+		super(n, s);
+		this.department = d;
+	}
+	
+	public void showInformation() {
+		
+		System.out.println("이름:" + this.getName() + " 연봉:" + this.getSalary() + " 부서:" + department);
+	}
+	
 }
